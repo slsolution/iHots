cf7862d73a7b532c19a15a70af47096b8a2720db
Rafael Developer <rafael@slsolution.com.br>
2017-01-20 20:25:28 -0200

    primeiro commit
    ...

---
+++ www/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>HOT's - Seu par naquelas horas</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no" />
+    
+    <meta name="description" content="Site especializado em encontro íntimos de pessoas. Aqui você consegue contatar ou ser contatado da forma mais segura e discreta possível.">
+    <meta name="keywords" content="encontro,homem,mulher,transsexual,íntimo,discreto,rápido,fácil,prazeroso" />
+
+    <style type="text/css">
+      @import url(css/main.css);
+
+    </style>
+  </head>
+  <body>
+
+   <header>
+      <h1 class="float-l">
+        <a href="#" title="Titulo do Site">iHots</a>
+      </h1>
+      
+      <input type="checkbox" id="control-nav" />
+      <label for="control-nav" class="control-nav"></label>
+      <label for="control-nav" class="control-nav-close"></label>
+
+      <nav class="float-r">
+        <ul class="list-auto">
+          <li>
+            <a href="#Chaves" title="Entra no sistema">Entrar</a>
+          </li>
+          <li>
+            <a href="#Chiquinha" title="Interesso-me por mulheres">Mulheres</a>
+          </li>
+          <li>
+            <a href="#Seu-Madruga" title="Interesso-me por homens ">Homens</a>
+          </li>
+          <li>
+            <a href="#Quico" title="Interesso-me por Transsexuais">Transsexuais</a>
+          </li>
+          <li>
+            <a href="#Quico" title="Contato">Contato</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+   
+
+    
+  </body>
+</html>
